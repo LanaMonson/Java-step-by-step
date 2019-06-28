c43b2fb6d9a4c21d930971c50d55f22c8b0a478a
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-27 19:11:17 -0600

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -7,16 +7,29 @@ public class ForLoop {
 }
 
 //TERMINAL OUTPUT:
-/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java "-javaagent:/Applications/IntelliJ IDEA.app/Contents/lib/idea_rt.jar=62070:/Applications/IntelliJ IDEA.app/Contents/bin" -Dfile.encoding=UTF-8 -classpath /Users/svetlanamonson/projects/java-steps/out/production/cycle ForLoop
-Hello 0
-Hello 1
-Hello 2
-Hello 3
-Hello 4
-Hello 5
-Hello 6
-Hello 7
-Hello 8
-Hello 9
-Hello 10
+///Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java "-javaagent:/Applications/IntelliJ IDEA.app/Contents/lib/idea_rt.jar=62070:/Applications/IntelliJ IDEA.app/Contents/bin" -Dfile.encoding=UTF-8 -classpath /Users/svetlanamonson/projects/java-steps/out/production/cycle ForLoop
+//Hello 0
+//Hello 1
+//Hello 2
+//Hello 3
+//Hello 4
+//Hello 5
+//Hello 6
+//Hello 7
+//Hello 8
+//Hello 9
+//Hello 10
 
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i<=10; i = i+5) {
+            System.out.println("Hello " +i);   // i++ ==  i = i+1 (true)
+        }
+    }
+}
+
+//TERMINAL OUTPUT:
+//Hello 0
+//Hello 5
+//Hello 10
