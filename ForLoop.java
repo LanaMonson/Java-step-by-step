26f2e85a77971b0f47881bd8c7f2c736ca86676d
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-27 18:53:51 -0600

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -5,3 +5,18 @@ public class ForLoop {
         }
     }
 }
+
+//TERMINAL OUTPUT:
+/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java "-javaagent:/Applications/IntelliJ IDEA.app/Contents/lib/idea_rt.jar=62070:/Applications/IntelliJ IDEA.app/Contents/bin" -Dfile.encoding=UTF-8 -classpath /Users/svetlanamonson/projects/java-steps/out/production/cycle ForLoop
+Hello 0
+Hello 1
+Hello 2
+Hello 3
+Hello 4
+Hello 5
+Hello 6
+Hello 7
+Hello 8
+Hello 9
+Hello 10
+
