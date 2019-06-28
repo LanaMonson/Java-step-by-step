db7e74c9dab6d2a82052e9cf6853741e9a4d14cc
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-27 19:14:31 -0600

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -33,3 +33,17 @@ public class ForLoop {
 //Hello 0
 //Hello 5
 //Hello 10
+
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 10; i>=0; i = i-5) {
+            System.out.println("Hello " +i);   // i++ ==  i = i+1 (true)
+        }
+    }
+}
+
+//TERMINAL OUTPUT:
+//Hello 10
+//Hello 5
+//Hello 0
