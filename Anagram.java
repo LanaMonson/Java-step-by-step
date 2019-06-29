b1950c93bf58d7b20d5587c3677f892943261999
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-29 07:51:36 -0600

    Create Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Anagram {
+    public static final String TYPE1 = "MyTest";
+    private final String type2;
+
+    public Anagram(String type2) {
+        this.type2 = type2;
+    }
+
+    public String getType2() {
+        return this.type2;
+    }
