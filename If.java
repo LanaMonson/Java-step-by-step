b4d9de145f9bce1ccf6f59925b34c9b479683b2c
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-29 10:01:58 -0600

    Update If.java

--- If.java
+++ If.java
@@ -30,3 +30,19 @@ public class If {
 }
 //TERMINAL OUTPUT:
 //No, it is wrong.
+
+
+public class If {
+    public static void main(String[] args) {
+        int myInt = 15;
+        if(myInt<10) {
+            System.out.println("Yes, it is correct.");
+        } else if(myInt>20){
+            System.out.println("No, it is wrong.");
+        } else{
+            System.out.println("Doesn't much the task requirements.");
+        }
+    }
+}
+//TERMINAL OUTPUT:
+//Doesn't much the task requirements.
