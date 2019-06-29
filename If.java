1fa9af825e8fb58625806201f29b1b9a3d51a122
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-29 09:34:16 -0600

    Update If.java

--- If.java
+++ If.java
@@ -5,3 +5,17 @@ public class If {
         }
     }
 }
+//TERMINAL OUTPUT:
+//Yes, it is correct!
+
+
+public class If {
+    public static void main(String[] args) {
+        if(5>2) {
+            System.out.println("Yes, it is correct!");
+        }
+    }
+}
+//TERMINAL OUTPUT:
+//Yes, it is correct!
+
