c9329591059aa0f9a874c51c02d8bae641051293
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-30 10:14:49 -0600

    Update DoWhile.java

--- DoWhile.java
+++ DoWhile.java
@@ -18,3 +18,28 @@ public class DoWhile {
 
 //5
 //You enter 5
+
+
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public  static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value;
+        do{
+            System.out.println("Enter 5");
+            value = scanner.nextInt();
+        } while(value!=5);
+        System.out.println("You enter 5");
+    }
+}
+
+//TERMINAL OUTPUT:
+//Enter 5
+//3
+//Enter 5
+//4
+//Enter 5
+//5
+//You enter 5
