5c3c489f5fd35300ae80490000e28aa4c84e5756
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-03 05:40:57 -0600

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+
+//public class Switch {
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the age");
+//        int age = scanner.nextInt();
+//        if(age == 10){
+//            System.out.println("You are schoolkid");
+//        }else if(age == 19){
+//            System.out.println("You are student");
+//        }
+//    }
+//}
