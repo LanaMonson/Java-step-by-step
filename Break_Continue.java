7a24492f004602644c156b6928843ce218f4b217
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-03 05:27:42 -0600

    Update Break_Continue.java

--- Break_Continue.java
+++ Break_Continue.java
@@ -15,3 +15,22 @@ public class Break_Continue {
 
 
 //CONTINUE
+public class Break_Continue {
+    public static void  main(String[] args) {
+        for(int i = 0; i<=15; i++){
+            if( i%2 ==0){
+                continue;
+            }
+            System.out.println("This is odd number"+i);
+        }
+    }
+}
+//TERMINAL OUTPUT:
+// This is odd number1
+// This is odd number3
+// This is odd number5
+// This is odd number7
+// This is odd number9
+// This is odd number11
+// This is odd number13
+// This is odd number15
