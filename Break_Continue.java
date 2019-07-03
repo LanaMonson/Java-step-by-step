2e95248cdab09d7ebfdec418a58fb6d669023f40
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-03 05:20:09 -0600

    Create Break_Continue.java

---
+++ Break_Continue.java
@@ -0,0 +1,17 @@
+//BREAK
+public class Break_Continue {
+    public static void  main(String[] args) {
+        int i = 0;
+        while(true) {
+            if(i == 15) {
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Cycle has complete");
+    }
+}
+
+
+//CONTINUE
