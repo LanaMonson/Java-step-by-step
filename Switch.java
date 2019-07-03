e24119ffddaf8f7dd5c2cb7cba02bc3ae1cbddfb
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-03 05:49:39 -0600

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -12,3 +12,28 @@
 //        }
 //    }
 //}
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the age");
+        int age = scanner.nextInt();
+        switch(age){
+            case 0:
+                System.out.println("You just born");
+                break;
+            case 7:
+                System.out.println("you are schoolkid");
+                break;
+            case 19:
+                System.out.println("You are student");
+                break;
+            default:
+                System.out.println("Does not match the parameters");
+        }
+    }
+}
+
+// TERMINAL OUTPUT:
