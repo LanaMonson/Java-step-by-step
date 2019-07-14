3000df8da439c2cd2794f05e0c55b60b141fbbd0
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-14 10:31:57 -0600

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -16,3 +16,32 @@ public class Arrays {
 //20
 //30
 //40
+
+
+public class Arrays {
+    public static void main(String[] args) {
+        int number = 10; //primitive variable //[10]
+        int [] numbers = new int[5];     //numbers ->[arrays] reference variable
+        for(int i = 0; i<numbers.length; i++){
+            numbers[i] =  i*10;
+        }
+        for(int i = 0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+        System.out.println();
+        int[] numbers1 = {1,2,3};
+        for(int i = 0; i<numbers1.length; i++){
+            System.out.println(numbers1[i]);
+        }
+    }
+}
+//TERMINAL OUTPUT:
+//0
+//10
+//20
+//30
+//40
+
+//1
+//2
+//3
