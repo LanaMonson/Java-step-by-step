f22419a4e5d7c5fc00a9fdb250b1bc1843b14dea
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-15 21:46:56 -0600

    Create Arrays_of_Strings.java

---
+++ Arrays_of_Strings.java
@@ -0,0 +1,13 @@
+public class Arrays_of_strings {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0]=10;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        
+    }
+}
+//TERMINAL OUTPUT:
+//10
+//0
+
