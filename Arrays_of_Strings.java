ab758be696cb1d48960435e3182bd1f2a63a7c0b
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-15 21:54:45 -0600

    Update Arrays_of_Strings.java

--- Arrays_of_Strings.java
+++ Arrays_of_Strings.java
@@ -11,3 +11,20 @@ public class Arrays_of_strings {
 //10
 //0
 
+
+public class Arrays_of_strings {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0]=10;
+        String[] strings = new String[3];
+        strings[0] = "Hello";
+        strings[1] = "Hooray";
+        strings[2] = "Java";
+        System.out.println(strings[0]);
+        System.out.println(strings[2]);
+    }
+}
+//TERMINAL OUTPUT:
+//Hello
+//Java
+
