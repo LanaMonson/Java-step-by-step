6d0fa4ad70f6337dc6d08a1624d0db4a697d04d9
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-16 19:30:00 -0600

    Create ClassesAndObjects.java

---
+++ ClassesAndObjects.java
@@ -0,0 +1,12 @@
+public class ClassesAndObjects {
+    public static void main(String[] args){
+        Person person1 = new Person();
+        person1.name = "John Doe";
+        person1.age = 50;
+    }
+}
+
+class Person{
+    String name;
+    int age;
+}
