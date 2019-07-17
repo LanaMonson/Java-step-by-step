fb85c2aac326bab51d6928dc3903a2826f61a156
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-16 19:37:22 -0600

    Update ClassesAndObjects.java

--- ClassesAndObjects.java
+++ ClassesAndObjects.java
@@ -3,6 +3,11 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "John Doe";
         person1.age = 50;
+        System.out.println("My name is "+ person1.name + "and I'm " + person1.age+" years old");
+        Person person2 = new Person();
+        person2.name = "Jane Smith";
+        person2.age = 20;
+
     }
 }
 
