40265e58aabcae0fd0ad6f7bb4d8aaed0e90d781
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-17 06:21:55 -0600

    Update ClassesAndObjects.java

--- ClassesAndObjects.java
+++ ClassesAndObjects.java
@@ -46,3 +46,32 @@ class Person{
 //My name is Richard Roe and I'm 45 years old
 //My name is Jane Doe and I'm 27 years old
 
+
+public class ClassesAndObjects {
+    public static void main(String[] args){
+        Person person1 = new Person();
+        person1.name = "Richard Roe";
+        person1.age = 45;
+        person1.speak();
+        Person person2 = new Person();
+        person2.name = "Jane Doe";
+        person2.age = 27;
+
+    }
+}
+
+class Person{
+    String name;
+    int age;
+
+    void speak(){
+        for(int i = 0; i<3; i++){
+        System.out.println("My name is "+ name + " and I'm " + age+" years old");
+        }
+    }
+}
+//TERMINAL OUTPUT:
+//My name is Richard Roe and I'm 45 years old
+//My name is Richard Roe and I'm 45 years old
+//My name is Richard Roe and I'm 45 years old
+
