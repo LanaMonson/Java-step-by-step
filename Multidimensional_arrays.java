1b1909076b82988a8b4f999f6cd8001fefe5e092
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-16 18:36:27 -0600

    Update Multidimensional_arrays.java

--- Multidimensional_arrays.java
+++ Multidimensional_arrays.java
@@ -61,3 +61,25 @@ public class Multidimensional_arrays {
 //4
 //5
 //Hello
+
+
+public class Multidimensional_arrays {
+    public static void main(String[] args){
+        int[] number = {1,2,3};
+
+        int[][] matrix = {{1,2,3},
+                          {4,5,6},
+                          {7,8,9}};
+
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = 0; j<matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+//TERMINAL OUTPUT:
+//1 2 3 
+//4 5 6 
+//7 8 9 
