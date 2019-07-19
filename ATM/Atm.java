ac5af38fba156643a1256288b555b917598e52b0
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-18 20:30:16 -0600

    Update Atm.java

--- ATM/Atm.java
+++ ATM/Atm.java
@@ -2,4 +2,8 @@ public class Atm {
     void methodIn(int card){
         System.out.println("Insert your card");
     }
+
+    void methodIn(short pin){
+        System.out.println("Enter pin code");
+    }
 }
