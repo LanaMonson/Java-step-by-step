8588cac305c744c0ea554662fbcd776f850e3062
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-18 21:26:18 -0600

    Update and rename Inheritance.java to Lesson25_Inheritance.java

---
+++ Inheritance/Lesson25_Inheritance.java
@@ -0,0 +1,12 @@
+public class Lesson25_Inheritance {
+    public static void main(String[] args){
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        dog.bark();
+    }
+}
+//TERMINAL OUTPUT:
+//om-nom-nom-nom
+// Z-zzz
+// Woof-Woof!
