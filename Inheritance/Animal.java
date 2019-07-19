c98b73f089700aeebf1a5b5564434c5f84cc11aa
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-18 21:27:09 -0600

    Update and rename Inheritance/Inheritance/Animal.java to Inheritance/Animal.java

---
+++ Inheritance/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public void eat(){
+        System.out.println("om-nom-nom-nom");
+    }
+
+    public void sleep(){
+        System.out.println("Z-zzz");
+    }
+}
--- Inheritance/Inheritance/Animal.java
+++
@@ -1,9 +0,0 @@
-public class Animal {
-    public void eat(){
-        System.out.println("I am eating");
-    }
-
-    public void sleep(){
-        System.out.println("I am sleeping");
-    }
-}
