5960940bfa9f25aacd82de790f0a4e59a157b1c5
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-22 19:46:35 -0600

    Update Test.java

--- Interfaces/Test.java
+++ Interfaces/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args){
         Animal animal1 = new Animal(1);
         Person person1 = new Person("Bob");
-        animal1.sleep();
-        person1.sayHello();
+        animal1.showInfo();
+        person1.showInfo();
     }
 }
