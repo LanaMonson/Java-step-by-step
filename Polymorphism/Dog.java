02f789652e9e751c4d42fa416ca660b25d24b4cf
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-24 13:00:36 -0600

    Create Dog.java

---
+++ Polymorphism/Dog.java
@@ -0,0 +1,12 @@
+package Polymorphism;
+
+public class Dog extends Animal {
+
+    public void eat(){
+        System.out.println("Crunch - Crunch!");
+    }
+
+    public void bark(){
+        System.out.println("Woof-Woof!");
+    }
+}
