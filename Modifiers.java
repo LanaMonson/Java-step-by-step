d628ad52d18f1f0e3dc77527fcadd2a7ca45ca9d
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-26 22:33:11 -0600

    Update Modifiers.java

--- Modifiers.java
+++ Modifiers.java
@@ -4,3 +4,10 @@
 //public Access Modifier.
 //Class Access Modifiers.
 //Interface Access Modifiers.
+
+//Access Modifiers
+
+//For classes, you can use either public or default:
+//public	The class is accessible by any other class
+//default	The class is only accessible by classes in the same package. This is used when you don't specify a modifier.
+
