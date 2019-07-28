d67065ea51e137b345760e0654dc5b798381d8dd
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-07-28 17:44:52 -0600

    Update Final.java

--- Final.java
+++ Final.java
@@ -1,2 +1,16 @@
 public class Final {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Test{
+    public final int CONSTANT;
+
+    public Test(int CONSTANT){
+        this.CONSTANT = CONSTANT;
+    }
+    public void setCONSTANT(int x){
+        this.CONSTANT = x;
+    }
 }
